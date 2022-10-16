32ccd3aa1d67b16925c56f7065148a0955fc307b
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-10-16 03:56:08 -0300

    cria método post

--- exercicios/projeto-casa/index.js
+++ exercicios/projeto-casa/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3333
+const clientesBancarios = require('./model/contas-clientes.json')
+const { v4: uuidv4 } = require('uuid')
 
 app.use(express.json)
+//console.log(uuidv4())
+
 // Criar os clientes do banco
+app.post('/clientes/novo', (req, res) => {
+  const { nome_cliente, cpf_cliente, data_nascimento, conta } = req.body
+  const idCliente = uuidv4()
+
+  const cadastraCliente = {
+    id: idCliente,
+    nome_cliente,
+    cpf_cliente,
+    data_nascimento,
+    conta
+  }
+  //console.log(cadastraCliente)
+  clientesBancarios.push(cadastraCliente)
+  return res.json(cadastraCliente)
+})
 // Atualizar informações desses clientes ( como endereço, telefone de contato...)
 // Fazer depósitos / pagamentos usando o saldo de sua conta
 // Encerrar contas de clientes
