7431017d868b8ff25eab7ac5d9678938243f3e59
mellyssaStephanny <mellyssa.smendes@gmail.com>
2022-10-16 14:59:30 -0300

    adiciona método delete

--- exercicios/projeto-casa/index.js
+++ exercicios/projeto-casa/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3333
-const clientesBancarios = require('./model/contas-clientes.json')
+const listaClientesBancarios = require('./model/contas-clientes.json')
 const { v4: uuidv4 } = require('uuid')
 
 app.use(express.json)
@@ -10,22 +10,38 @@ app.use(express.json)
 // Criar os clientes do banco
 app.post('/clientes/novo', (req, res) => {
   const { nome_cliente, cpf_cliente, data_nascimento, conta } = req.body
-  const idCliente = uuidv4()
+  //const idCliente = uuidv4()
 
   const cadastraCliente = {
-    id: idCliente,
+    id: uuidv4(),
     nome_cliente,
     cpf_cliente,
     data_nascimento,
     conta
   }
-  //console.log(cadastraCliente)
-  clientesBancarios.push(cadastraCliente)
-  return res.json(cadastraCliente)
+  listaClientesBancarios.push(cadastraCliente)
+  return res.status(201).json(cadastraCliente)
 })
 // Atualizar informações desses clientes ( como endereço, telefone de contato...)
 // Fazer depósitos / pagamentos usando o saldo de sua conta
+
 // Encerrar contas de clientes
+app.delete('/clientes/:id', (req, res) => {
+  const idCliente = req.params.id
+  const deletarCliente = listaClientesBancarios.find(
+    cliente => cliente.id == idCliente
+  )
+
+  if (deletarCliente) {
+    listaClientesBancarios.map((cliente, index) => {
+      if (cliente.id == idCliente) {
+        listaClientesBancarios.splice(index, 1)
+      }
+    })
+    return res.status(200).json(listaClientesBancarios)
+  }
+  return res.status(404).json({ message: 'Cliente não encontrado!!' })
+})
 // Conseguir Filtrar os clientes do banco pelo seu nome,por saldo...
 
 app.listen(port, () => {
