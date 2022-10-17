582c092b150b212c7f4f017cb78aa20f211e8af4
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-10-17 20:57:18 -0300

    Add atualizações

---
+++ exercicios/projeto-casa/index.js
@@ -0,0 +1,5 @@
+//Criar os clientes do banco
+//Atualizar informações desses clientes ( como endereço, telefone de contato...)
+//Fazer depósitos / pagamentos usando o saldo de sua conta
+//Encerrar contas de clientes
+//Conseguir Filtrar os clientes do banco pelo seu nome,por saldo...
