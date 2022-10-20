7dd3f35f8cd67f30042f3a651b66a4cbfb52f8ab
Karina Carvalho <karina@blacknetsolucoes.com.br>
2022-10-20 20:23:43 -0300

    Projeto Guiado II Alteraçao 1

--- exercicios/projeto-casa/projeto-Karina/index.js
+++ exercicios/projeto-casa/projeto-Karina/index.js
@@ -1,6 +1,36 @@
-const express = require('express');
-const app = express();
+const express = require('express');  //Foi declarado (importado)
+const app = express();  //Foi inicializado (instânciado)
 const port = 3000;
-const listaClientes = require('./model/consultas-clientes.json');
+const listaClientes = require('./model/contas-clientes.json'); //Foi declarado lista de cliente e mostrado qual caminho
 
 app.use(express.json());
+
+//Criar os clientes do banco
+
+app.post("/")
+
+//Atualizar informações desses clientes ( como endereço, telefone de contato...)
+app.patch("/")
+
+//Fazer depósitos / pagamentos usando o saldo de sua conta
+app.patch("/")
+
+
+//Encerrar contas de clientes
+app.delete("/")
+
+
+//Conseguir Filtrar os clientes do banco pelo seu nome,por saldo...
+app.get("/")
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`API está rodando na porta ${port}`);
+  });
