4c38326c78c8625f218e4ba5185430592646748f
Beatriz Silva <bps.beatrizsilva@gmail.com>
2022-12-17 15:05:39 -0300

    atualização index

--- exercicios/projeto-casa/index.js
+++ exercicios/projeto-casa/index.js
@@ -1,45 +1,158 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const port = 3000
-const listaClientes = require("./model/contas-clientes.json")
-app.use(express.json())
+const port = 3000;
+const { v4: uuidv4 } = require("uuid");
+const newAccount = Math.floor(Math.random() * 30000000);
+const contasClientes = require("./model/contas-clientes.json");
+
+app.use(express.json());
+
+app.post("/conta/add", (req, res) => {
+  const {
+    nome_cliente,
+    cpf_cliente,
+    data_nascimento,
+    conta: { tipo },
+  } = req.body;
+  const existeCPF = contasClientes.find(
+    (conta) => conta.cpf_cliente == cpf_cliente
+  );
+
+  if (!existeCPF) {
+    const newCliente = {
+      id: uuidv4(),
+      nome_cliente,
+      cpf_cliente,
+      data_nascimento,
+      conta: {
+        numero: newAccount,
+        tipo,
+        saldo: 0,
+        data_criacao: new Date().toISOString(),
+     
+      },
+    };
+    contasClientes.push(newCliente);
+    return res.status(201).json(contasClientes);
+  }
+  return res.status(404).json({
+    messagem: `Cliente com CPF: ${cpf_cliente} Conta já cadastrada`,
+  });
+});
 
 
+app.patch("/conta/:id/consulta", (req, res) => {
+  const idCliente = req.params.id;
+  const { telefone: newNumber } = req.body;
 
-app.get('/listaclientes', (req, res) => {
-    res.json(listaClientes)
-}) 
+  const existeCliente = contasClientes.find((conta) => conta.id == idCliente);
 
-// - Criar os clientes do banco
-app.post("/cadastro",(req, res) => {
-    const body = req.body
-    listaClientes.push(body) // o push adiciona um novo item a array
-    res.json(listaClientes)
-    
-})
+  if (existeCliente) {
+    const atualizarCliente = contasClientes.map((conta) => {
+      conta.telefone = newNumber;
 
-// Atualizar informações desses clientes ( como endereço, telefone de contato...)
+      return { ...conta };
+    });
+    return res.status(200).json(atualizarCliente);
+  }
+  return res
+    .status(404)
+    .json({ messagem: "Cliente não foi encontrado." });
+});
 
-app.patch("clientes/:cpf_cliente", (req, res) => {
-    const CPFcliente = req.params.cpf_cliente
-    const atualizacao = req.body
-    
-    const novaListaClientes = listaClientes.map((clientes)=> {
-        if(clientes.id = CPFcliente) {
-            return {
-                    ...clientes, ...atualizacao
-            }
-        }
-        return clientes
-    })
-    res.status(200).json(novaListaClientes) //status code 200 indica que a requisição foi bem sucedida
-})
+app.patch("/conta/:id/deposito", (req, res) => {
+  const idCliente = req.params.id;
+  const { deposito } = req.body;
+
+  const clienteExiste = contasClientes.find(
+    (cliente) => cliente.id == idCliente
+  );
+
+  if (clienteExiste) {
+    const efetuarDeposito = {
+      ...clienteExiste.conta,
+      saldo: clienteExiste.conta.saldo + deposito,
+    };
+
+    contasClientes.map((cliente, index) => {
+      if (cliente.id == idCliente) {
+        contasClientes[index].conta = efetuarDeposito;
+      }
+    });
+    return res.status(200).json(contasClientes);
+  }
+  return res.status(404).json({
+    messagem:
+      "Erro no depósito. Tente novamente.",
+  });
+});
+
+app.patch("/conta/:id/pagamento", (req, res) => {
+  const idCliente = req.params.id;
+  const { pagamento } = req.body;
+
+  const clienteExiste = contasClientes.find(
+    (cliente) => cliente.id == idCliente
+  );
+
+  if (clienteExiste.conta.saldo >= pagamento) {
+    const efetuarPagamento = {
+      ...clienteExiste.conta,
+      saldo: clienteExiste.conta.saldo - pagamento,
+    };
+
+    contasClientes.map((cliente, index) => {
+      if (cliente.id == idCliente) {
+        contasClientes[index].conta = efetuarPagamento;
+      }
+    });
+    return res.status(200).json(contasClientes);
+  }
+  return res.status(400).json({
+    messagem:
+      "Pagamento maior que o saldo",
+  });
+});
 
 
-// Fazer depósitos / pagamentos usando o saldo de sua conta
-// Encerrar contas de clientes
-// Conseguir Filtrar os clientes do banco pelo seu nome,por saldo...
+app.delete("/conta/deletar/:id", (req, res) => {
+  const idCliente = req.params.id;
+
+  const existeCliente = contasClientes.find((conta) => conta.id == idCliente);
+
+  if (existeCliente) {
+    contasClientes.map((conta, index) => {
+      if (conta.id == idCliente) {
+        return contasClientes.splice(index, 1);
+      }
+    });
+    return res.status(200).json(contasClientes);
+  }
+  return res.status(404).json({
+    message: "O cliente não encontrado",
+  });
+});
+
+app.get("/conta/filtros", (req, res) => {
+  const filtrarNome = req.params.nome;
+  const filtrarNascimento = req.params.data;
+  const filtrarSaldo = parseFloat(req.params.saldo);
+
+  const prodFiltros = contasClientes.find((item) => {
+    if (filtrarNome) {
+      return item.nome_cliente.toLowerCase() == filtrarNome.toLowerCase();
+    }
+    if (filtrarNascimento) {
+      return item.data_nascimento == filtrarNascimento;
+    }
+    if (filtrarSaldo) {
+      return item.conta.saldo == filtrarSaldo;
+    }
+    return item;
+  });
+  return res.status(200).json(prodFiltros);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`API está rodando na porta ${port}`);
+});
